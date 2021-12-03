556022a87e4451eb4dd118c8c73c382f4d4821cf
lokeshmk25 <lokeshmk2211@gmail.com>
2021-12-03 19:22:19 +0530

    Added: GetMapping method to get String message

--- src/main/java/com/bridgelabz/greetingsapplication/GreetingsApplication.java
+++ src/main/java/com/bridgelabz/greetingsapplication/GreetingsApplication.java
@@ -3,6 +3,11 @@ package com.bridgelabz.greetingsapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lokesh
+ * @since 03-12-2021
+ * Purpose - To create Greetings Application using Rest Api
+ */
 @SpringBootApplication
 public class GreetingsApplication {
 
--- src/main/java/com/bridgelabz/greetingsapplication/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingsapplication/controller/GreetingController.java
@@ -1,12 +1,25 @@
 package com.bridgelabz.greetingsapplication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author lokesh
+ * @since 03-12-2021
+ * Purpose - To demonstrate different HTTP methods
+ */
 @RestController
 public class GreetingController {
+
     @RequestMapping("/greeting")
     public String greet(){
         return "Welcome to my project";
+    }
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello from lokesh";
     }
 }
